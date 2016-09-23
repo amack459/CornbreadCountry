769d55ac00cad71572a0606afd8bf291391abd46
Acacia Mack <acacialmack@gmail.com>
2016-09-23 16:41:07 +0100

    added content centered footer

---
+++ js/app.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+
+  var about = document.getElementById("about-content");
+  var reviews = document.getElementById("reviews-content");
+
+  about.style.display = "none";
+  reviews.style.display = "none";
+  
+  var tabs = document.getElementsByClassName("tabs");
+
+  var content = document.getElementsByClassName("tab_content");
+
+
+
+  for(var i = 0; i <tabs.length; i++){
+    tabs[i].addEventListener("click", function(){
+     for(var j = 0; j<content.length; j++){
+      content[j].style.display = "none";
+     }
+     document.getElementById(this.id + "-content").style.display = "block";
+    })
+
+  }
+}
